1c7fc4f3cfaef30ff94f7f3f72c247b35a26cce5
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-04-08 12:26:57 +0200

    maquetacion

--- dist/whether.js
+++ dist/whether.js
@@ -3,11 +3,30 @@ function getWhether() {
     fetch('https://www.el-tiempo.net/api/json/v2/provincias/08/municipios/08019')
         .then(res => res.json())
         .then(datos => {
-        console.log(datos.stateSky);
+        let myDatos = { temperatura: datos.temperatura_actual, description: datos.stateSky.description };
+        let { temperatura, description } = myDatos;
         let whetBox = document.getElementById('whetBox');
         if (whetBox !== null) {
-            whetBox.innerHTML = `${datos.temperatura_actual} - ${datos.stateSky.description}`;
+            whetBox.innerHTML = `${putIcon(description)}  ${`<h4>|    ${temperatura}°</h4>`}`;
         }
     });
 }
 getWhether();
+function putIcon(str) {
+    switch (str) {
+        case 'Nubes altas':
+            return '<img src="./img-whether/cloudy.svg">';
+            break;
+        case 'Despejado':
+            return '<img src="./img-whether/day.svg">';
+            break;
+        case 'Poco nuboso':
+            return '<img src="./img-whether/cloudy-day-2.svg">';
+            break;
+        case 'Cubierto con lluvia escasa':
+            return '<img src="./img-whether/rainy-2.svg">';
+            break;
+        default:
+            return '<img src="./img-whether/cloudy-day-3.svg">';
+    }
+}
